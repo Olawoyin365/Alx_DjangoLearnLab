9fa463db9e72b789797b85793648750e9f2d1ac3
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-01 08:10:41 +0000

    tried out practical experience with the Django admin interface by configuring the admin to manage the Book model.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Book
 
-admin.ModelAdmin.register(Book)
+# admin.ModelAdmin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
 
 # Register your models here.
