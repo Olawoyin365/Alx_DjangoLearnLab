e8bbf5774b202cbf2ebd43615d1697115f34d529
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-09 19:13:59 +0000

    This is a project that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields, and the use of function and class based views.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,14 +4,13 @@ from django.views.generic import DetailView
 from .models import Book, Library
 
 
-#Function-based view — List all books
+#Function-based view; List all books
 def book_list(request):
     books = Book.objects.select_related('author').all()
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-#Class-based view — Library detail with its books
+#Class-based view; Library detail with its books
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
