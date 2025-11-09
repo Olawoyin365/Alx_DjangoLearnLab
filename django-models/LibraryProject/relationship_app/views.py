4a560448b9a71606fb22d3da39878fe229a91e1d
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-09 20:39:25 +0000

    This is a project that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields, and the use of function and class based views.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 #Function-based view; List all books
