52f694a5c1c400ca2fdf0519d372d3f4a203baba
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-09 21:12:48 +0000

    This is a project that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields, and the use of function and class based views.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 
 
 #Function-based view; List all books
@@ -27,3 +28,31 @@ def log_out(request):
 def register(request):
     UserCreationForm()
     return render(request, 'relationship_app/register.html')
+
+#Helper functions to check user roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+#Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+#Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+#Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
