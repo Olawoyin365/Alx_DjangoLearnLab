44d8f2440b58ee640ecbb7f0eabc12f0b8a0c2b9
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-09 20:53:38 +0000

    This is a project that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields, and the use of function and class based views.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,8 +6,8 @@ from .views import list_books
 urlpatterns = [
     path('book_list/', views.book_list),
     path('library_detail/', views.LibraryDetailView.as_view()),
-    path('login/', views.login),
-    path('logout/', views.logout),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html')),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html')),
     path('register/', views.register),
 
 ]
