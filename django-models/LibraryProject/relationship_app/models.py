902c3a2930773d171667beccad9e549e43bc142d
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-09 21:24:46 +0000

    This is a project that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields, and the use of function and class based views.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,12 +10,28 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
+"""class Book(models.Model):
     title = models.CharField
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title"""
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    library = models.ForeignKey('Library', on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 class Library(models.Model):
     name = models.CharField
