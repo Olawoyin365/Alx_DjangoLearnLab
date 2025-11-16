78770e73bac56e7debd92a3c963a7261122283a6
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-16 18:22:59 +0000

    security measures in your Django project to protect against common vulnerabilities

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.contenttypes.models import ContentType
 from django.contrib import admin
 from .models import Book, CustomUser
 
@@ -42,3 +46,30 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publication_year']
     list_filter = ['publication_year', 'author']
     search_fields = ['title', 'author']
+
+@receiver(post_migrate)
+def create_default_groups(sender, **kwargs):
+    if sender.name != "accounts" and sender.name != "your_app_name":
+        return  # avoid running for irrelevant apps
+
+    # Get Book model permissions
+    content_type = ContentType.objects.get_for_model(Book)
+
+    can_view = Permission.objects.get(codename="can_view", content_type=content_type)
+    can_create = Permission.objects.get(codename="can_create", content_type=content_type)
+    can_edit = Permission.objects.get(codename="can_edit", content_type=content_type)
+    can_delete = Permission.objects.get(codename="can_delete", content_type=content_type)
+
+    # Create groups
+    editors, created = Group.objects.get_or_create(name="Editors")
+    viewers, created = Group.objects.get_or_create(name="Viewers")
+    admins, created = Group.objects.get_or_create(name="Admins")
+
+    # Assign permissions
+    viewers.permissions.set([can_view])
+
+    editors.permissions.set([can_view, can_create, can_edit])
+
+    admins.permissions.set([
+        can_view, can_create, can_edit, can_delete
+    ])
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
+@permission_required('your_app_name.can_view', raise_exception=True)
+def book_list(request):
+    pass
+
+@permission_required('your_app_name.can_create', raise_exception=True)
+def create_book(request):
+    pass
+
+
+@permission_required('your_app_name.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    pass
+
+
+@permission_required('your_app_name.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    pass
 # Create your views here.
