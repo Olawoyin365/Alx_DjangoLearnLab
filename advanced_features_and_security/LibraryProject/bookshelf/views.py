5770903b8e0e501481b69f7b10eb1e564939ea23
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-16 18:48:03 +0000

    security measures in your Django project to protect against common vulnerabilities

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,21 +1,85 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from .models import Book
 
-@permission_required('your_app_name.can_view', raise_exception=True)
+# View to create new book - requires can_create permission
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # SECURE: Using validated and cleaned form data
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            publication_year = form.cleaned_data['publication_year']
+            
+            Book.objects.create(
+                title=title,
+                author=author,
+                publication_year=publication_year
+            )
+            messages.success(request, 'Book created successfully!')
+            return redirect('book_list')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/book_create.html', {'form': form})
+
+# View to edit existing book - requires can_edit permission
+@login_required
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # SECURE: Using validated and cleaned form data
+            book.title = form.cleaned_data['title']
+            book.author = form.cleaned_data['author']
+            book.publication_year = form.cleaned_data['publication_year']
+            book.save()
+
+            messages.success(request, 'Book updated successfully!')
+            return redirect('book_detail', pk=book.pk)
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        # Pre-populate form with existing book data
+        form = ExampleForm(initial={
+            'title': book.title,
+            'author': book.author,
+            'publication_year': book.publication_year
+        })
+
+    return render(request, 'bookshelf/book_edit.html', {'form': form, 'book': book})
+
+# Book list view - requires can_view permission
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    pass
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
-@permission_required('your_app_name.can_create', raise_exception=True)
-def create_book(request):
-    pass
+# Book detail view - requires can_view permission
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'bookshelf/book_detail.html', {'book': book})
 
-
-@permission_required('your_app_name.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    pass
-
-
-@permission_required('your_app_name.can_delete', raise_exception=True)
-def delete_book(request, pk):
-    pass
-# Create your views here.
+# Book delete view - requires can_delete permission
+@login_required
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, 'Book deleted successfully!')
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
