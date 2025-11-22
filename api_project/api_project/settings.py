6f04c16f4753758a4309627322e3ad03e29ebd1b
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-22 03:23:13 +0000

    This is a ptoject to integrate DRF and prepare for creating API endpoints

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-auzz#=y&n&yvx#9!)hdt9iv5eket0yz5w!k4-f+ift7j8zb-8&'
+SECRET_KEY = 'django-insecure-l@1#pzl0l8!13b+c#o_##g688h!)cu5i4g-mh*-s@!l!pgqt7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
