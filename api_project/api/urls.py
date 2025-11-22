ac3b48aca66abf6d56f66daf485ee5f620b6ac3f
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-22 04:14:38 +0000

    a basic API endpoint to list all books using Django REST Framework.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from resr_framework.routers import DefaultRouter
 from .views import BookList
+
+
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
