ff4c215446d33eea699c0c4ae8d6514fee58de53
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-27 08:25:44 +0000

    custom views using Django REST Framework’s powerful generic views and mixins.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
-# Create your views here.
+# Set of generic views for the Book model that handles CRUD operations.
+
+class BookList(ListAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetail(RetrieveAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookCreate(CreateAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookUpdate(UpdateAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookDelete(DestroyAPIView):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
