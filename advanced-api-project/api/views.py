987a83f9e72cdcfd4d7d9abf4871669d5199f979
Ibrahim Olawoyin <woyin365@gmail.com>
2025-11-30 02:35:51 +0000

    advanced query capabilities with API endpoints to filter, search, and order the books based on different criteria.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,30 +3,40 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Set of generic views for the Book model that handles CRUD operations.
 
 class BookListView(ListAPIView):
-    query_set = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+            DjangoFilterBackend,
+            SearchFilter,
+            OrderingFilter
+            ]
+    filterset_fields = ['title', 'author', 'publication_yeat']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 
 class BookDetailView(RetrieveAPIView):
-    query_set = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookCreateView(CreateAPIView):
-    query_set = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 class BookUpdateView(UpdateAPIView):
-    query_set = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 class BookDeleteView(DestroyAPIView):
-    query_set = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
